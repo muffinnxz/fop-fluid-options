48cfdc37ca4ec781a6b796947b455c89b6c8ed10
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-09-12 18:10:42 +0700

    warning: this code is not success yet,I can't flow

--- frontend/pages/call/[optionAddress].js
+++ frontend/pages/call/[optionAddress].js
@@ -3,10 +3,12 @@ import { ethers } from "ethers";
 import { useRouter } from "next/router";
 import { useAccount, useProvider, useSigner } from "wagmi";
 import TradeableCallOption from "../../../contracts/artifacts/contracts/TradeableCallOption.sol/TradeableCallOption.json";
+import { Framework } from "@superfluid-finance/sdk-core";
 
 export default function User() {
     const router = useRouter();
     const { address, isConnected } = useAccount();
+    const { data: signer } = useSigner();
     const [optionAddress, setOptionAddress] = useState();
     const [optionData, setOptionData] = useState();
 
@@ -37,7 +39,54 @@ export default function User() {
 
     const approveUnderlyingAsset = async () => {};
 
-    const createFlow = async () => {};
+    const createFlow = async () => {
+        console.log("Start create flow...");
+        const provider = new ethers.providers.JsonRpcProvider(
+            "https://eth-goerli.g.alchemy.com/v2/wZc5RVdqBMtlqlLySTdU4RcAxY2FcUAI"
+        );
+        const sf = await Framework.create({
+            chainId: 5,
+            provider: provider,
+        });
+        const contract = new ethers.Contract(
+            router.query.optionAddress,
+            TradeableCallOption.abi,
+            ethers.getDefaultProvider("goerli")
+        );
+
+        const DAIxContract = await sf.loadSuperToken("fDAIx");
+        const DAIx = DAIxContract.address;
+
+        try {
+            const flowRate = await contract._requiredFlowRate();
+            console.log(
+                "FlowRate:",
+                flowRate,
+                ethers.BigNumber.from(flowRate).toString()
+            );
+            const createFlowOperation = sf.cfaV1.createFlow({
+                flowRate: ethers.BigNumber.from(flowRate).toString(),
+                receiver: router.query.optionAddress,
+                superToken: DAIx,
+            });
+
+            console.log("Creating your stream...");
+
+            const signer = sf.createSigner({
+                privateKey:
+                    "0xd2ebfb1517ee73c4bd3d209530a7e1c25352542843077109ae77a2c0213375f1",
+                provider: provider,
+            });
+
+            const result = await createFlowOperation.exec(signer);
+            console.log(result);
+        } catch (error) {
+            console.log(
+                "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+            );
+            console.error(error);
+        }
+    };
 
     return (
         <div className=" h-screen">
