e7da823b412cbd89b682b26df506b49bc36bf924
patkamon <patkamon.a@ku.th>
2022-09-18 10:43:08 +0700

    add myoption

--- frontend/pages/my-option/index.js
+++ frontend/pages/my-option/index.js
@@ -2,5 +2,26 @@ import Call from "../../components/Call";
 import CreateOption from "../../components/CreateOption";
 
 export default function User() {
-  return <CreateOption />;
+  return (
+    <div>
+      <section>
+        Create Option:
+        <CreateOption />
+      </section>
+
+      <section>
+        Seller:
+        <br /> Call:
+        <br /> Put:
+        <br />
+      </section>
+      <br />
+      <section>
+        Buyer:
+        <br /> Call:
+        <br /> Put:
+        <br />
+      </section>
+    </div>
+  );
 }
