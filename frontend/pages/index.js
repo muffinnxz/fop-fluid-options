b6174f651892e7a09b8fc49b0ebafa9755d59588
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-09-24 20:18:23 +0700

    remove unused import

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,11 @@
 import styles from "../styles/Home.module.css";
-import { ethers } from "ethers";
-import OptionPutFactory from "../../contracts/artifacts/contracts/OptionPutFactory.sol/OptionPutFactory.json";
 import { useState } from "react";
-import Web3 from "web3";
 import { toast } from "react-hot-toast";
 
-import Call from "../components/Call";
-import Card from "../components/Card";
-import PaginatedItems from "../components/CallPagination";
 import PaginatedCallItems from "../components/CallPagination";
 import PaginatedPutItems from "../components/PutPagination";
 
 export default function Home() {
-  const CallFactoryAddress = "0xca0BF23f1Ea4E08ea053691C0Dd0C066b0c31665";
-  const PutFactoryAddress = "0x264569c1325C26e41832dE6C8D978d59fCb05D60";
-  const fDAIx = "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00";
-  const dai = "0x88271d333C72e51516B67f5567c728E702b3eeE8";
 
   const [filterCall, setFilterCall] = useState(true);
   const [filterPut, setFilterPut] = useState(false);
